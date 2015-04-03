66de227e308824fee299ebfa4d4f78f32248c2cf
Jake Robinson <jarobins139@gmail.com>
2015-04-03 09:19:51 -0400

    Added Classes and Trace Test
    Added the Trace class for drawing real time data to the screen. Still need to test out the trace class with acctual streamed data from our sensor module. This test run data out of a pre-set list, but the real implementation should be similar.

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame
 import random
+import math
 
 # CLASSES
 
 class Button:
     hovered = False
     
-    def __init__(self, label, img_set, pos):
+    def __init__(self, label, img_set, pos, func):
         self.label = label
-        self.img_hov = img_set[1]
+        self.img_on = img_set[1]
         self.img_off = img_set[0]
         self.pos = pos
         self.set_rect()
         self.draw()
+        self.func = func
             
     def draw(self):
         self.set_rend()
@@ -23,16 +25,56 @@ class Button:
         
     def get_img(self):
         if self.hovered:
-            return self.img_clik
+            return self.img_on
         else:
             return self.img_off
         
     def set_rect(self):
         self.set_rend()
         self.rect = self.rend.get_rect()
-        self.rect.topleft = self.pos    
+        self.rect.topleft = self.pos
 
- 
+    def do_func(self):
+        self.func
+
+class Trace:
+    def __init__(self, label, color, y_pos):
+        self.label = label
+        self.color = color
+        self.y_pos = y_pos
+        self.draw_list = [(0,0),(0,0),(0,0),(0,0),
+                          (0,0),(0,0),(0,0),(0,0),
+                          (0,0),(0,0),(0,0),(0,0)]
+        self.count = 0
+            
+    def draw(self, func):
+        self.count += 1
+        self.draw_list.append((func, self.count))
+        self.draw_list = self.draw_list[1:]
+        for item in reversed(range(1,12)):
+            # draws a line between the data points
+            # (x_pos1,
+            if (self.draw_list[item][1]%size[0])-(self.draw_list[item-1][1]%size[0]) < 0:
+                continue  # Skips the line that draws across the screen
+            pygame.draw.line(screen, self.color,
+                             (self.draw_list[item][1]%size[0],
+                              self.y_pos-self.draw_list[item][0]),
+                             (self.draw_list[item-1][1]%size[0],
+                              self.y_pos-self.draw_list[item-1][0]))
+            
+        
+
+def draw_thing():
+    print "thing"
+    pygame.draw.rect(screen, (255, 0, 255), (random.randint(1, 255),
+                              random.randint(1, 255),
+                              random.randint(1, 255),
+                              random.randint(1, 255)))
+    
+
+def draw_something():
+    pass
+
 # initialize game engine
 pygame.init()
 # set screen width/height and caption
@@ -46,16 +88,23 @@ clock = pygame.time.Clock()
 myfont = pygame.font.SysFont("monospace", 15)
 
 # Sprites
-OK_hov = pygame.image.load("./OK.png")
-OK_off = pygame.image.load("./START.png")
+OK_on = pygame.image.load("./OK.png")
+OK_off = pygame.image.load("./OK_on.png")
+START_on = pygame.image.load("./START.png")
+START_off = pygame.image.load("./START_on.png")
 
-buttons = [Button("OK", (OK_hov, OK_off), (10, 10)),
-           Button("START", (OK_off, OK_hov), (10, 40)),]
+buttons = [Button("OK", (OK_on, OK_off), (10, 200), draw_thing()),
+           Button("START", (START_on, START_off), (100, 200), draw_something())]
+traces = [Trace("Heart_Beats", (255, 0, 0), 170),]
+
 # Define Varibles
 x, y, a, b = 1, 1, 2, 2
+data_list = [0,0,0,0,0,0,0,-1,-3,-4,-4,-2,5,15,20,23,
+             25,25,24,20,14,6,-3,-5,-6,-5,-3,-2,-1,0,0,0,0]
  
 # Loop until the user clicks close button
 done = False
+count = 0
 while done == False:
     # write event handlers here
     for event in pygame.event.get():
@@ -63,13 +112,13 @@ while done == False:
             if event.key == pygame.K_ESCAPE:
                 done = True
         # handle MOUSEBUTTONUP
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             print pos
             # get a list of all sprites that are under the mouse cursor
             clicked_buttons = [s for s in buttons if s.rect.collidepoint(pos)]
             if clicked_buttons:
-                print clicked_buttons[0].label
+                clicked_buttons[0].do_func
                 
     # write game logic here
  
@@ -78,7 +127,14 @@ while done == False:
     
     # write draw code here
     for button in buttons:
+        if button.rect.collidepoint(pygame.mouse.get_pos()):
+            button.hovered = True
+        else:
+            button.hovered = False
         button.draw()
+    for trace in traces:
+        count += 1
+        trace.draw(data_list[count%len(data_list)])
     # display whats drawn. this might change.
     pygame.display.update()
     # run at 20 fps
